8ab2261162ec5de109a63ded669cc076dfa6c8bb
Manish999999999 <manishkhatri9199@gmail.com>
2026-02-27 14:12:58 +0545

    Add Contact structure definition in Contact.h

---
+++ structures/Contact.h
@@ -0,0 +1,11 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+// Contact structure – holds name, phone, email
+struct Contact {
+    char name[50];
+    char phone[15];
+    char email[50];
+};
+
+#endif
